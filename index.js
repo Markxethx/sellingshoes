5db806e6cd361e97d3d73c3424d5bd11a1a7a5d8
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2022-08-03 21:55:39 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let errorParagraph = document.getElementById("error")
+console.log(errorParagraph)
+
+function purchase(){
+    console.log("Button Clicked");
+    errorParagraph.textContent = "Something went wrong, please try again";
+} 
+ 
+
+/*
+let errorParagraph = document.getElementById("error")
+console.log(errorParagraph)
+
+function purchase() {
+    console.log("button clicked")
+    errorParagraph.textContent = "Something went wrong, please try again"
+} */
